bc425a90f611c547178c8419e48839d08efd7f85
ashlesha0017 <158022653+ashlesha0017@users.noreply.github.com>
2026-03-08 16:26:41 +0530

    Update Greet.java

--- Greet.java
+++ Greet.java
@@ -1,6 +1,8 @@
 public class Greet {
     public static void main(String[] args) {
         System.out.println("Hello from Java Pipeline");
+        System.out.println("Hello from Java Pipeline");
     }
     
 }
+
