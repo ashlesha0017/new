e440ff3a0065d0775d9952f4414c1dcd459bd83f
ashlesha0017 <158022653+ashlesha0017@users.noreply.github.com>
2026-03-08 14:50:54 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
     public static void main(String[] args){
-        System.out.println("hello,World!");
         System.out.println("Test commit for Multi Trigger Pipeline");
         System.out.println("Test commit for Multi Trigger Pipeline");
+        
     }
 }
