2c5d188b94fc2906d090c109fc8d4822f135b8e4
ashlesha0017 <158022653+ashlesha0017@users.noreply.github.com>
2026-03-08 14:15:58 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,8 +1,7 @@
 public class hello{
     public static void main(String[] args){
         System.out.println("hello,World!");
-        System.out.println("hello,World!");
-        System.out.println("Hello from Jenkins Poll SCM Updated");
         System.out.println("Test commit for Jenkins webhook trigger");
+        System.out.println("Test commit for Multi Trigger Pipeline");
     }
 }
